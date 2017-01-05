3b2595f754e54f755a356018c84f3cc4eed03dfa
谢文良 <765947965@qq.com>
2017-01-05 17:53:00 +0800

    画画板

--- app/src/main/java/app/wenya/sketchbookpro/ui/activity/SketchBookProActivity.java
+++ app/src/main/java/app/wenya/sketchbookpro/ui/activity/SketchBookProActivity.java
@@ -6,7 +6,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -14,7 +13,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 
-import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.rm.freedraw.FreeDrawView;
 
